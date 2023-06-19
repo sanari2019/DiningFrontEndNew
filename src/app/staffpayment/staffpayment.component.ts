68be7997f5148c76b39c9fd4674f38482443a757
sanari2019 <samachoo2010@icloud.com>
2023-06-19 14:55:01 +0100

    190623_sam

--- src/app/staffpayment/staffpayment.component.ts
+++ src/app/staffpayment/staffpayment.component.ts
@@ -23,107 +23,115 @@ export class StaffpaymentComponent implements OnInit {
 
   @ViewChildren(FormControlName, { read: ElementRef })
   formInputElements: ElementRef[] = [];
-  pageTitle="New Staff Ticket"
-  message='';
-  staffid=1;
-  errorMessage= '';
+  pageTitle = "New Staff Ticket"
+  message = '';
+  staffid = 1;
+  errorMessage = '';
   paymentForm!: FormGroup;
   private formSubmitAttempt!: boolean;
   vouchers!: Voucher[];
-  paymentmodes!:PaymentMode[];
+  paymentmodes!: PaymentMode[];
   payment!: Payment;
   public registration: Registration | undefined;
   private sub!: Subscription;
   private validationMessages!: { [key: string]: { [key: string]: string } };
   private genericValidator!: GenericValidator;
-  public dataFields:Object={text:'Value',value:'Id'};
-  Units: any = [1,2,3,4,5,6,7,8,9,10];
+  public dataFields: Object = { text: 'Value', value: 'Id' };
+  Units: any = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  constructor(private fbstaff: FormBuilder, private router: Router,private paymentmodeservice:PaymentModeService,private voucherservice:VoucherService, private paymentservice: PaymentService, private encdecservice:EncrDecrService) {
+  constructor(private fbstaff: FormBuilder, private router: Router, private paymentmodeservice: PaymentModeService, private voucherservice: VoucherService, private paymentservice: PaymentService, private encdecservice: EncrDecrService) {
 
-      this.validationMessages = {
-        employeeNo: {
-          required: 'Employee No is required.',
-          minlength: 'Employee No must be at least three characters.'
-        },
-      };
+    this.validationMessages = {
+      employeeNo: {
+        required: 'Employee No is required.',
+        minlength: 'Employee No must be at least three characters.'
+      },
+    };
 
-      this.genericValidator = new GenericValidator(this.validationMessages);
+    this.genericValidator = new GenericValidator(this.validationMessages);
 
 
-     }
+  }
 
-     ngOnInit(): void {
+  ngOnInit(): void {
 
-      this.paymentForm = this.fbstaff.group({
-        custCode: new FormControl('',[Validators.required,Validators.minLength(3)]),
-        voucherId: new FormControl('',[Validators.required,Validators.min(1)]),
-        paymentmodeid: new FormControl('',[Validators.required,Validators.min(1)]),
-        unit: new FormControl('', Validators.required)
+    this.paymentForm = this.fbstaff.group({
+      custCode: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      voucherId: new FormControl('', [Validators.required, Validators.min(1)]),
+      paymentmodeid: new FormControl('', [Validators.required, Validators.min(1)]),
+      unit: new FormControl('', Validators.required)
 
-      });
-      // this.voucherForm=this.fb2.group({
-      //   'id':[null]
-      // })
-      this.getVouchers();
-      this.getpaymentmodes();
+    });
+    // this.voucherForm=this.fb2.group({
+    //   'id':[null]
+    // })
+    // Access the locally stored user information
+    const user = localStorage.getItem('user');
+    this.registration = user ? JSON.parse(user) : undefined;
+    this.getVouchers();
+    this.getpaymentmodes();
+
+    // Populate custCode input field with user's ID as placeholder
+    const custCodeControl = this.paymentForm.get('custCode');
+    if (custCodeControl && this.registration) {
+      custCodeControl.setValidators([Validators.required, Validators.minLength(3)]);
+      custCodeControl.setValue(this.registration.id);
     }
-    isFieldInvalid(field: string) {
-      return (
-        (!this.paymentForm.get(field)?.valid && this.paymentForm.get(field)?.touched) ||
-        (this.paymentForm.get(field)?.untouched && this.formSubmitAttempt)
-       );
-    }
-    savePayment(): void {
-      // console.log(this.registrationForm);
-      // console.log('Saved: ' + JSON.stringify(this.registrationForm.value));
-      if (this.paymentForm.valid) {
-       if (this.paymentForm.dirty)
-       {
-          const p = { ...this.payment, ...this.paymentForm.value };
-           if (p.custCode !== '') {
-            p.dateEntered=new Date();
-            var loggeinuser = localStorage.getItem('user');
-            this.registration=loggeinuser !== null? JSON.parse(loggeinuser): new Registration();
-            p.enteredBy=this.registration?.id.toString();
-            p.custtypeid=1;
-            p.servedby="";
-            if (confirm(`You are about to generate a ticket for Staff: ${p.custCode}?`))
-            {
+  }
+  isFieldInvalid(field: string) {
+    return (
+      (!this.paymentForm.get(field)?.valid && this.paymentForm.get(field)?.touched) ||
+      (this.paymentForm.get(field)?.untouched && this.formSubmitAttempt)
+    );
+  }
+  savePayment(): void {
+    // console.log(this.registrationForm);
+    // console.log('Saved: ' + JSON.stringify(this.registrationForm.value));
+    if (this.paymentForm.valid) {
+      if (this.paymentForm.dirty) {
+        const p = { ...this.payment, ...this.paymentForm.value };
+        if (p.custCode !== '') {
+          p.dateEntered = new Date();
+          var loggeinuser = localStorage.getItem('user');
+          this.registration = loggeinuser !== null ? JSON.parse(loggeinuser) : new Registration();
+          p.enteredBy = this.registration?.id.toString();
+          p.custtypeid = 1;
+          p.servedby = "";
+          if (confirm(`You are about to generate a ticket for Staff: ${p.custCode}?`)) {
             this.paymentservice.createPayment(p)
-             .subscribe({
-               next: () => this.onSaveComplete(),
-              error: err => this.errorMessage = err
-            });
-            }
-          } else if (p.custCode === '') {
-            this.pageTitle="Enter Employee Code";
+              .subscribe({
+                next: () => this.onSaveComplete(),
+                error: err => this.errorMessage = err
+              });
+          }
+        } else if (p.custCode === '') {
+          this.pageTitle = "Enter Employee Code";
           //  this.paymentservice.updatePayment(p)
           //  .subscribe({
           //    next: () => this.onSaveComplete(),
           //     error: err => this.errorMessage = err
           //    });
-         }
         }
-        // else
-        // {
-        //  this.onSaveComplete();
-        // }
-       }
-       else {
-         this.errorMessage = 'Please correct the validation errors.';
-       }
+      }
+      // else
+      // {
+      //  this.onSaveComplete();
+      // }
     }
-      getVouchers() {
-        this.voucherservice.getVoucher(this.staffid).subscribe(res => this.vouchers = res, error => this.errorMessage = <any>error);
-      }
+    else {
+      this.errorMessage = 'Please correct the validation errors.';
+    }
+  }
+  getVouchers() {
+    this.voucherservice.getVoucher(this.staffid).subscribe(res => this.vouchers = res, error => this.errorMessage = <any>error);
+  }
 
-      getpaymentmodes() {
-        this.paymentmodeservice.getPaymentModes().subscribe(res => this.paymentmodes = res, error => this.errorMessage = <any>error);
-      }
+  getpaymentmodes() {
+    this.paymentmodeservice.getPaymentModes().subscribe(res => this.paymentmodes = res, error => this.errorMessage = <any>error);
+  }
 
   onSaveComplete(): void {
     // this.registrationForm.reset();
     this.router.navigate(['/voucherdetails']);
-     }
-   }
+  }
+}
